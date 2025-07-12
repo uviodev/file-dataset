0d48ec19ca9db0f3ed4f35be4d04864cd5afb301
Patrick Violette <violette.patrick718@gmail.com>
2025-07-12 16:28:52 -0500

    Simplifying the scaffolding code a little.

--- src/file_dataset/__init__.py
+++ src/file_dataset/__init__.py
@@ -1,11 +1,5 @@
 try:
     from importlib.metadata import version, PackageNotFoundError
-    try:
-        __version__ = version("file-dataset")
-    except PackageNotFoundError:
-        __version__ = None
-except ImportError:
+    __version__ = version("file-dataset")
+except (ImportError, PackageNotFoundError):
     __version__ = None
-
-def main() -> None:
-    print("Hello from file-dataset!")
